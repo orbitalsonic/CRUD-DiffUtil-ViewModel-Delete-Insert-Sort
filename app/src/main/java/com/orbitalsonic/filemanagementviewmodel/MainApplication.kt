24d51e3d885371cbd6a83de007e5dcff7ade5014
Yaqoob <social.yaqoob@gmail.com>
2023-04-02 11:40:03 +0500

    File Management

---
+++ app/src/main/java/com/orbitalsonic/filemanagementviewmodel/MainApplication.kt
@@ -0,0 +1,20 @@
+package com.orbitalsonic.filemanagementviewmodel
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initKoin()
+    }
+
+    private fun initKoin() {
+        startKoin {
+            androidContext(this@MainApplication)
+            modules(modulesList)
+        }
+    }
+}
